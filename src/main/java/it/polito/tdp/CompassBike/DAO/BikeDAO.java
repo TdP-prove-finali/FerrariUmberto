1f8841ad5d8d83d0e1275b3754b0452b84b8b12c
Umberto Ferrari <s246061@studenti.polito.it>
2020-08-01 16:17:57 +0200

    Correction dataImport

--- src/main/java/it/polito/tdp/CompassBike/DAO/BikeDAO.java
+++ src/main/java/it/polito/tdp/CompassBike/DAO/BikeDAO.java
@@ -9,13 +9,15 @@ import it.polito.tdp.CompassBike.model.Rental;
 public class BikeDAO {
 	
 	public static void addBike(Rental rental) {
-		String sql = "INSERT INTO bike VALUES(?)";
+		String sql = "INSERT INTO bike VALUES(?, ?) ON DUPLICATE KEY UPDATE bike_id = ?";
 		Connection conn = DBConnect.getConnection();
 		
 	    try {
 			PreparedStatement st = conn.prepareStatement(sql);
 			
 			st.setInt(1, rental.getBikeId());
+			st.setInt(2, rental.getEndStationId());
+			st.setInt(3, rental.getBikeId());
 			
 			st.executeUpdate();
 			conn.close();
--- src/main/java/it/polito/tdp/CompassBike/DAO/RentalsDAO.java
+++ src/main/java/it/polito/tdp/CompassBike/DAO/RentalsDAO.java
@@ -10,14 +10,14 @@ import it.polito.tdp.CompassBike.model.Rental;
 public class RentalsDAO {
 
 	public static void addRental(Rental rental) {
-		System.out.println("QUI");
-		String sql = "INSERT INTO rentals VALUES(?, ?, ?, ?, ?, ?, ?)";
+		String sql = "INSERT INTO rentals VALUES(?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE rental_id = ?";
 		Connection conn = DBConnect.getConnection();
 		
 	    try {
 			PreparedStatement st = conn.prepareStatement(sql);
 			
 			st.setInt(1, rental.getId());
+			st.setInt(8, rental.getId());
 			st.setLong(2, rental.getDuration().toMillis());
 			st.setInt(3, rental.getBikeId());
 			st.setTimestamp(4, Timestamp.valueOf(rental.getEndDate()));
